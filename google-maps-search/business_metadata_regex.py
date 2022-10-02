1b0a8dc090fc5b472cbf3585b887bc9c9e208fb5
Matthew Cover <41007986+matthewncover@users.noreply.github.com>
2022-10-02 12:45:33 -0700

    remove state requirement, find dow

--- google-maps-search/business_metadata_regex.py
+++ google-maps-search/business_metadata_regex.py
@@ -6,13 +6,29 @@ def is_website(text):
 
     return True if bool(re.search(expression, text)) else False
 
+def is_address(text):
+
+    try:
+        text_split = text.split(",")
+        at_least_two_commas = (len(text_split) >= 3)
+        state_zip_exists = bool(re.search(r"\w{2}\s\d{5}", text_split[-1]))
+        return True if at_least_two_commas & state_zip_exists else False
+
+    except IndexError:
+        return False
+
+def get_state_from_address(text):
+
+    return re.search(r"\w{2}\s\d{5}", text.split(',')[-1]).group().strip()[:2]
+
 def is_state_address(text, state="AZ"):
 
     try:
         text_split = text.split(",")
         at_least_two_commas = (len(text_split) >= 3)
+        state_zip_exists = bool(re.search(r"\w{2}\s\d{5}", text_split[-1]))
         correct_state = (text_split[-1].strip()[:2] == state)
-        return True if at_least_two_commas & correct_state else False
+        return True if at_least_two_commas & correct_state & state_zip_exists else False
 
     except IndexError:
         return False
@@ -22,3 +38,8 @@ def is_phone_number(text):
     expression = r"[(]?\d{3}[)]?\s?\d+[-]?\d+"
 
     return True if bool(re.search(expression, text)) else False
+
+def find_day_of_week(text, days_of_week):
+
+    expression = rf"({'|'.join(days_of_week)})"
+    return re.search(expression, text).group()
