7d42fd88b768e52c92346a5fb5224914a49439d0
Matthew Cover <41007986+matthewncover@users.noreply.github.com>
2022-10-02 12:47:13 -0700

    add check for duplicates before business search

--- google-maps-search/maps_section_search.py
+++ google-maps-search/maps_section_search.py
@@ -1,17 +1,24 @@
+import pandas as pd
 from bs4 import BeautifulSoup
 
 from business_metadata import MapsBusiness
 
 class MapsSectionSearch:
 
-    def __init__(self, driver, state_abr, url):
+    def __init__(self, driver, state_abr, url, df=None):
 
         self.state_abr = state_abr
         self.driver = driver
         self.url = url
 
         self.get_soup()
-        self.get_businesses()
+        self.get_business_names_urls()
+
+        if df is not None:
+            self.df = df
+            self.check_data_for_duplicate()
+
+        self.get_business_data()
 
     def __repr__(self):
 
@@ -35,27 +42,38 @@ class MapsSectionSearch:
             features = "html.parser"
             )
 
-    def get_businesses(self):
-        """
-        """
+    def get_business_names_urls(self):
 
-        business_name_url_tuples = [
-            (x["aria-label"], x["href"])
-            for x in self.soup.find_all(
-                "a",
-                {"class": "hfpxzc"}
-            )
+        business_tags = self.soup.find_all(
+            "a", {"class": "hfpxzc"}
+        )
+
+        self.name_url_tuples = [
+            (bs4_tag["aria-label"], bs4_tag["href"])
+            for bs4_tag in business_tags
         ]
+
+    def get_business_data(self):
+        """
+        """
 
         self.businesses = [
             MapsBusiness(self.driver, self.state_abr, business_name, business_url)
             for (business_name, business_url)
-            in business_name_url_tuples
+            in self.name_url_tuples
         ]
 
-        self.businesses = [
-            business for business in self.businesses
-            if business.is_valid_state_business_flag
+        # self.businesses = [
+        #     business for business in self.businesses
+        #     if business.is_valid_state_business_flag
+        # ]
+
+    def check_data_for_duplicate(self):
+
+        self.name_url_tuples = [
+            (name, url) for (name, url)
+            in self.name_url_tuples
+            if [name, url] not in self.df[["business_name", "href_url"]].values.tolist()
         ]
 
 
