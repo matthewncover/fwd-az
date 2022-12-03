d9d7933855f6cce400ad96f8825322754eef55b0
Matthew Cover <41007986+matthewncover@users.noreply.github.com>
2022-12-03 15:57:08 -0700

    add address regex queries

--- google-maps-search/business_metadata_regex.py
+++ google-maps-search/business_metadata_regex.py
@@ -43,3 +43,42 @@ def find_day_of_week(text, days_of_week):
 
     expression = rf"({'|'.join(days_of_week)})"
     return re.search(expression, text).group()
+
+##
+
+def address_breakdown(text):
+
+    state_abr_expression = ("""
+    AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|
+    KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|
+    OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY
+    """).strip()
+
+    state_expression = ("""
+    Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|
+    Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|
+    Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New[ ]Hampshire|New[ ]Jersey|New[ ]Mexico|
+    New[ ]York|North[ ]Carolina|North[ ]Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode[ ]Island|
+    South[ ]Carolina|South[ ]Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West[ ]Virginia|
+    Wisconsin|Wyoming
+    """).strip()
+
+    city_expression = ("""
+    (?:[A-Z][a-z.-]+[ ]?)+
+    """).strip()
+
+    zipcode_expression = ("""
+    \d{5}(?:-\d{4})?
+    """).strip()
+
+    try:
+        address_elements = re.search(rf"({city_expression}),[ ](?:({state_abr_expression}))[ ]({zipcode_expression})", text)
+        city, state, zipcode = address_elements.groups()
+        
+        return city, state, zipcode
+
+    except (AttributeError, TypeError):
+        return None, None, None
+
+
+
